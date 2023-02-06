63c36395d685e3954e08b3b3f14a8f2ef713d558
Scrollbutton <123376590+Scrollbutton@users.noreply.github.com>
2023-02-06 18:52:28 +0300

    Add files via upload

---
+++ 20230206-172056.py
@@ -0,0 +1,16 @@
+#bonus
+salary=int(input("enter salary:"))
+time_of_service=int(input("enter time_of_service:"))
+if time_of_service>10:
+   bonus=0.1*salary
+   print("net_bonus:",bonus+salary)
+elif time_of_service>=6 and time_of_service<=10:
+   bonus=0.08*salary
+   print("net_bonus:",bonus+salary) 
+elif time_of_service<6:
+   bonus=0.05*salary
+   print("net_bonus:",bonus+salary)
+
+
+
+  

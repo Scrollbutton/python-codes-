03e20ff9d4e14ec57c2ba05bacf5447b22a1d882
Scrollbutton <123376590+Scrollbutton@users.noreply.github.com>
2023-02-06 19:22:03 +0300

    Add files via upload

---
+++ 20230206-190314.py
@@ -0,0 +1,12 @@
+#multple test
+num=int(input("Enter a number:"))
+if num%5==0:
+   print("Hello")
+else:
+   print("Bye")
+
+
+
+
+  
+    
---
+++ 20230206-191614.py
@@ -0,0 +1,16 @@
+#login
+username="mary"
+password="123"
+input_username=input("Enter username:")
+input_password=input("Enter password:")
+if username==input_username and password==input_password:
+  print ("login successful")
+else: 
+  print ("Invalid credentials")
+
+
+
+
+
+  
+    
